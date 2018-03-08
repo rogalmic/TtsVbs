39df069f582b95f4d7206d22430646fbda30e368
Michał <rogalinsky@gmail.com>
2018-03-08 11:44:13 +0100

    No extra interface

--- webcam.start.bat
+++ webcam.start.bat
@@ -3,5 +3,5 @@ REM https://wiki.videolan.org/Documentation:Modules/mosaic/
 @set CurrentPath=%~dp0
 @cd "%CurrentPath%"
 
-start "" "C:\Program Files (x86)\VideoLAN\VLC\vlc.exe" --vlm-conf .\webcams.vlm.conf --mosaic-width=1280 --mosaic-height=720 --mosaic-keep-picture --mosaic-rows=2 --mosaic-cols=2  --mosaic-order=1,2,3,4 --image-duration=-1 
+start "" "C:\Program Files (x86)\VideoLAN\VLC\vlc.exe" --intf dummy --vlm-conf .\webcams.vlm.conf --mosaic-width=1280 --mosaic-height=720 --mosaic-keep-picture --mosaic-rows=2 --mosaic-cols=2  --mosaic-order=1,2,3,4 --image-duration=-1 
 @endlocal
